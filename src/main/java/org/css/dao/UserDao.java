2d28aede006d21813ddf878f45cb0cf0268c2ee0
Liam Cahill <cahill.liam220@gmail.com>
2023-09-15 12:42:57 -0400

    Add new SQL for Account creation. createAccount() in progress.

--- src/main/java/org/css/dao/UserDao.java
+++ src/main/java/org/css/dao/UserDao.java
@@ -1,5 +1,7 @@
 package org.css.dao;
 
+import org.css.model.Account;
+
 public interface UserDao {
 
     int createAccount(Account account);
---
+++ src/main/java/org/css/dao/UserDaoJDBC.java
@@ -0,0 +1,39 @@
+package org.css.dao;
+
+import org.css.controller.MyConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import org.css.model.Account;
+
+public class UserDaoJDBC {
+
+    private static Logger LOGGER = Logger.getLogger(String.valueOf(UserDaoJDBC.class));
+
+    @Override
+    public int createAccount(Account account){
+        try (Connection connection = MyConnection.getConnection()) {
+            int index = 0;
+
+            String sql1 = "SELECT * FROM ACCOUNT WHERE A_EMAIL = ?";
+            PreparedStatement statement1 = connection.prepareStatement(sql1);
+            statement1.setString(1, account.getUsername());
+            if (statement1.executeUpdate() > 0) {
+                System.out.println("Account already exists.");
+
+                return 0;
+            } else {
+                //Add logic to initialize new account creation
+                String sql = "INSERT INTO ACCOUNT()";
+                System.out.println("-----Creating your new account-----");
+
+
+        }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+}
