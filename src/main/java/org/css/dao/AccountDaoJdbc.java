a78515f2f449326e9e738f51fcf8d559e7786a67
Liam Cahill <cahill.liam220@gmail.com>
2023-10-08 14:00:16 -0400

    Troubleshooting PreparedStatement errors.

--- src/main/java/org/css/dao/AccountDaoJdbc.java
+++ src/main/java/org/css/dao/AccountDaoJdbc.java
@@ -2,13 +2,13 @@ package org.css.dao;
 
 import org.css.controller.MyConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.css.model.Account;
+
+import javax.xml.transform.Result;
 
 public class AccountDaoJdbc implements AccountDao {
 
@@ -17,37 +17,49 @@ public class AccountDaoJdbc implements AccountDao {
     public int createAccount(Account account) {
         LOGGER.log(Level.INFO, "Creating account from AccountDaoJdbc createAccount method.");
         try (Connection connection = MyConnection.getConnection()) {
-            LOGGER.log(Level.INFO, "Inside AccountDaoJdbc createAccount connection try block");
-
             int index = 0;
-            String sql1 = "SELECT * FROM USERS WHERE U_USERNAME = ?";
+            String usersQueryForUser = "SELECT * FROM USERS WHERE U_USERNAME = ?";
             LOGGER.log(Level.INFO, "sql SELECT statement initialized.");
 
-            PreparedStatement statement1 = connection.prepareStatement(sql1);
-            statement1.setString(1, account.getUsername());
+            PreparedStatement stmt = connection.prepareStatement(usersQueryForUser);
+            stmt.setString(1, account.getUsername());
+            ResultSet rs = stmt.executeQuery(usersQueryForUser);
 
-            // TODO:  executeUpdate() or executeLargeUpdate() with statements that produce result sets
-            if (statement1.executeUpdate()> 0) {
+            if (rs.next()){
                 LOGGER.log(Level.INFO, "Account was found in database already. Will not be creating new one.");
                 System.out.println("Account already exists.");
                 return 0;
             } else {
                 LOGGER.log(Level.INFO, "Account was not found in database. Creating new one.");
-                String sql2 = "INSERT INTO ACCOUNT(U_USERNAME, U_EMAIL, U_PASSWORD) VALUES (?,?,?)";
-                PreparedStatement statement = connection.prepareStatement(sql2);
-
-                statement.setString(++index, account.getUsername());
-                statement.setString(++index, account.getEmail());
-                statement.setString(++index, account.getPassword());
-
-
-                if (statement.executeUpdate() > 0) {
-                    LOGGER.log(Level.INFO, "New account successfully created.");
-                    System.out.println("Success.");
-                    return 0;
-                }
-                LOGGER.log(Level.INFO, "New account creation failed..");
+                System.exit(1);
             }
+
+//            PreparedStatement statement1 = connection.prepareStatement(sql1);
+//            LOGGER.log(Level.INFO, "Checking for user " + account.getUsername() + " in database.");
+//            statement1.setString(1, account.getUsername());
+
+            // TODO:  executeUpdate() or executeLargeUpdate() with statements that produce result sets
+//            if (usersQueryForUser.executeQuery() != null) {
+//                LOGGER.log(Level.INFO, "Account was found in database already. Will not be creating new one.");
+//                System.out.println("Account already exists.");
+//                return 0;
+//            } else {
+//                LOGGER.log(Level.INFO, "Account was not found in database. Creating new one.");
+//                String sql2 = "INSERT INTO ACCOUNT(U_USERNAME, U_EMAIL, U_PASSWORD) VALUES (?,?,?)";
+//                PreparedStatement statement = connection.prepareStatement(sql2);
+//
+//                statement.setString(++index, account.getUsername());
+//                statement.setString(++index, account.getEmail());
+//                statement.setString(++index, account.getPassword());
+//
+//
+//                if (statement.executeUpdate() > 0) {
+//                    LOGGER.log(Level.INFO, "New account successfully created.");
+//                    System.out.println("Success.");
+//                    return 0;
+//                }
+//                LOGGER.log(Level.INFO, "New account creation failed..");
+//            }
         } catch (SQLException e) {
             LOGGER.log(Level.SEVERE, e.toString());
         }
